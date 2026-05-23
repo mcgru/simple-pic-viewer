a59d0957a31b36d6ac5a04e9161c04974d681776
Ivan I. Grushin <mcgru@cgames.team>
2026-05-23 20:45:28 +0700

    chore: bump version to 1.3.1

--- makedeb.sh
+++ makedeb.sh
@@ -8,7 +8,7 @@
 set -e
 
 PKG_NAME="simple-pic-viewer"
-PKG_VERSION="1.3.0"
+PKG_VERSION="1.3.1"
 PKG_ARCH="amd64"
 DEB_FILE="${PKG_NAME}_${PKG_VERSION}_${PKG_ARCH}.deb"
 
--- version.v
+++ version.v
@@ -1,3 +1,3 @@
 module main
 
-pub const app_version = '1.3.0'
+pub const app_version = '1.3.1'
